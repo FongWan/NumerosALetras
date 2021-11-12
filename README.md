20b36f58ded6e7a115cd540af02fb43f6183d41f
Fong-Wan Chau <fongwan@chaurocks.com>
2021-11-12 02:21:55 -0300

    Initial commit
    Signed-off-by: Fong-Wan Chau <fongwan@chaurocks.com>

---
+++ README.md
@@ -0,0 +1,5 @@
+# NúmerosALetras
+
+Esta aplicación web gratuito te permite convertir los números a letras en tiempo real, gracias al uso de javascript.
+
+No requiere que estés conectado al internet (una vez abierto la página), ni almacena datos.
---
+++ number2word.js
@@ -0,0 +1,71 @@
+(function () {
+// According to RAE, the first thirties is really special
+let firstThirties = ['', 'un', 'dos', 'tres', 'cuatro', 'cinco', 'seis', 'siete', 'ocho', 'nueve', 'diez', 'once', 'doce', 'trece', 'catorce', 'quince', 'dieciséis', 'diecisiete', 'dieciocho', 'diecinueve', 'veinte', 'veintiún', 'veintidós', 'veintitrés', 'veinticuatro', 'veinticinco', 'veintiséis', 'veintisiete', 'veintiocho', 'veintinueve'];
+let lastTens = ['', '', '', 'treinta', 'cuarenta', 'cincuenta', 'sesenta', 'setenta', 'ochenta', 'noventa'];
+let hundreds = ['', 'cien', 'doscientos', 'trescientos', 'cuatrocientos', 'quinientos', 'seiscientos', 'setecientos', 'ochocientos', 'novecientos'];
+let millions = ['', 'millón', 'billón', 'trillón', 'cuatrillón', 'quintillón', 'sextillón', 'septillón', 'octillón', 'nonillón', 'decillón', 'undecillón', 'duodecillón', 'tredecillón', 'cuatordecillón', 'quindecillón', 'sexdecillón', 'septendecillón', 'octodecillón', 'novendecillón', 'vigintillón'];
+let fractions = ['', 'décimo', 'centécimos', 'milésimos', 'diezmilésimos', 'cienmilésimos', 'millonésimos', 'diezmillonésimos', 'cienmillonésimos', 'billonésimos'];
+
+let thousand2word = function (number) {
+	number = number.toString();
+	if (6 < number.length) return false;
+	number = number.padStart(6, '0').match(/^(\d{1})(\d{2})(\d{1})(\d{2})$/).slice(1).reverse();
+
+	let str = [];
+	if (0 != number[0]) {
+		if (firstThirties[Number(number[0])]) str.push(firstThirties[Number(number[0])]);
+		else if (firstThirties[number[0][1]]) str.push(lastTens[number[0][0]] + ' y ' + firstThirties[number[0][1]]);
+		else                                  str.push(lastTens[number[0][0]]);
+	}
+	if (0 != number[1]) {
+		if (0 != number[0] && 1 == number[1]) str.push(hundreds[number[1]] + 'to ');
+		else                                  str.push(hundreds[number[1]] + ' ');
+	}
+	if (0 != number[2]) {
+		if (1 == Number(number[2]))                str.push('mil ');
+		else if (firstThirties[Number(number[2])]) str.push(firstThirties[Number(number[2])] + ' mil ');
+		else if (firstThirties[number[2][1]])      str.push(lastTens[number[2][0]] + ' y ' + firstThirties[number[2][1]] + ' mil ');
+		else                                       str.push(lastTens[number[2][0]] + ' mil ');
+	}
+	if (0 != number[3]) {
+		if (0 != number[2] && 1 == number[3]) str.push(hundreds[number[3]] + 'to ');
+		else                                  str.push(hundreds[number[3]] + ' ');
+	}
+	if (0 == str.length) str.push('cero');
+
+	return str.reverse().join('').trim();
+};
+
+let wholenumber2word = function (number) {
+	// Group by 6 digits from the end
+	number = number.toString();
+	let groups = number.match(/(\d{1,6})(?=(\d{6})+(?!\d))/g);
+	if (!groups) groups = [];
+	groups.push(number.slice(-6));
+
+	// Create words from each group and add the millions unit if needed
+	let str = [];
+	groups.reverse().forEach(function (item, index) {
+		if (1 < Number(item) && index) str.push((thousand2word(item) + ' ' + millions[index].substr(0, millions[index].length - 2) + 'ones '));
+		else                           str.push((thousand2word(item) + ' ' + millions[index] + ' '));
+	});
+	return str.reverse().join('').trim();
+};
+
+window.number2word = function (number) {
+	// Fix problem of big numbers
+	number = number.toLocaleString('fullwide', {useGrouping:false});
+	if (parseFloat(number) > Math.pow(10, 120)) return false;
+
+	// Split into two parts
+	number = number.split('.', 2);
+	str = [wholenumber2word(number[0])];
+	if (2 == number.length) {
+		str.push(' con ' + wholenumber2word(number[1]) + ' ');
+		let length = number[1].length;
+		if (1 == length && Number(number[1]) > 1) str.push(fractions[length] + 's');
+		else str.push(fractions[length]);
+	}
+	return str.join('');
+};
+})();
