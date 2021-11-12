58b0991b576448b83667e1dcb02b1cda95661681
Fong-Wan Chau <fongwan@chaurocks.com>
2021-11-12 03:24:48 -0300

    Bug fixes
    Signed-off-by: Fong-Wan Chau <fongwan@chaurocks.com>

--- number2word.js
+++ number2word.js
@@ -1,10 +1,10 @@
 (function () {
 // According to RAE, the first thirties is really special
-let firstThirties = ['', 'un', 'dos', 'tres', 'cuatro', 'cinco', 'seis', 'siete', 'ocho', 'nueve', 'diez', 'once', 'doce', 'trece', 'catorce', 'quince', 'dieciséis', 'diecisiete', 'dieciocho', 'diecinueve', 'veinte', 'veintiún', 'veintidós', 'veintitrés', 'veinticuatro', 'veinticinco', 'veintiséis', 'veintisiete', 'veintiocho', 'veintinueve'];
-let lastTens = ['', '', '', 'treinta', 'cuarenta', 'cincuenta', 'sesenta', 'setenta', 'ochenta', 'noventa'];
-let hundreds = ['', 'cien', 'doscientos', 'trescientos', 'cuatrocientos', 'quinientos', 'seiscientos', 'setecientos', 'ochocientos', 'novecientos'];
-let millions = ['', 'millón', 'billón', 'trillón', 'cuatrillón', 'quintillón', 'sextillón', 'septillón', 'octillón', 'nonillón', 'decillón', 'undecillón', 'duodecillón', 'tredecillón', 'cuatordecillón', 'quindecillón', 'sexdecillón', 'septendecillón', 'octodecillón', 'novendecillón', 'vigintillón'];
-let fractions = ['', 'décimo', 'centécimos', 'milésimos', 'diezmilésimos', 'cienmilésimos', 'millonésimos', 'diezmillonésimos', 'cienmillonésimos', 'billonésimos'];
+let firstThirties = ['', 'un', 'dos', 'tres', 'cuatro', 'cinco', 'seis', 'siete', 'ocho', 'nueve', 'diez', 'once', 'doce', 'trece', 'catorce', 'quince', 'dieciséis', 'diecisiete', 'dieciocho', 'diecinueve', 'veinte', 'veintiún', 'veintidós', 'veintitrés', 'veinticuatro', 'veinticinco', 'veintiséis', 'veintisiete', 'veintiocho', 'veintinueve'],
+    lastTens = ['', '', '', 'treinta', 'cuarenta', 'cincuenta', 'sesenta', 'setenta', 'ochenta', 'noventa'],
+    hundreds = ['', 'cien', 'doscientos', 'trescientos', 'cuatrocientos', 'quinientos', 'seiscientos', 'setecientos', 'ochocientos', 'novecientos'],
+    millions = ['', 'millón', 'billón', 'trillón', 'cuatrillón', 'quintillón', 'sextillón', 'septillón', 'octillón', 'nonillón', 'decillón', 'undecillón', 'duodecillón', 'tredecillón', 'cuatordecillón', 'quindecillón', 'sexdecillón', 'septendecillón', 'octodecillón', 'novendecillón', 'vigintillón'],
+    fractions = ['', 'décimo', 'centécimo', 'milésimo', 'diezmilésimo', 'cienmilésimo'];
 
 let thousand2word = function (number) {
 	number = number.toString();
@@ -31,7 +31,6 @@ let thousand2word = function (number) {
 		if (0 != number[2] && 1 == number[3]) str.push(hundreds[number[3]] + 'to ');
 		else                                  str.push(hundreds[number[3]] + ' ');
 	}
-	if (0 == str.length) str.push('cero');
 
 	return str.reverse().join('').trim();
 };
@@ -46,10 +45,14 @@ let wholenumber2word = function (number) {
 	// Create words from each group and add the millions unit if needed
 	let str = [];
 	groups.reverse().forEach(function (item, index) {
+		if (0 == Number(item)) return;
 		if (1 < Number(item) && index) str.push((thousand2word(item) + ' ' + millions[index].substr(0, millions[index].length - 2) + 'ones '));
 		else                           str.push((thousand2word(item) + ' ' + millions[index] + ' '));
 	});
-	return str.reverse().join('').trim();
+	str = str.reverse().join('').trim();
+	if (0 == str.length) str = 'cero';
+
+	return str;
 };
 
 window.number2word = function (number) {
@@ -60,11 +63,31 @@ window.number2word = function (number) {
 	// Split into two parts
 	number = number.split('.', 2);
 	str = [wholenumber2word(number[0])];
-	if (2 == number.length) {
+	if (2 == number.length && Number(number[1])) {
 		str.push(' con ' + wholenumber2word(number[1]) + ' ');
 		let length = number[1].length;
-		if (1 == length && Number(number[1]) > 1) str.push(fractions[length] + 's');
-		else str.push(fractions[length]);
+
+		if (fractions[length]) str.push(fractions[length]);
+		else {
+			length -= 3;
+			let millionsIndex = Math.floor(length / 3),
+			    millionsRemain = length % 3;
+
+			if (!millions[millionsIndex]) return false;
+
+			switch (millionsRemain) {
+				case 1:
+					str.push('diez');
+					break;
+				case 2:
+					str.push('cien');
+					break;
+			}
+
+			str.push(millions[millionsIndex].substr(0, millions[millionsIndex].length - 2) + 'onésimo');
+		}
+
+		if (Number(number[1]) > 1) str.push('s');
 	}
 	return str.join('');
 };
